350f9e7747e1d0589294ea6f28d734c86d68baa7
David Nin <dninmur@gmail.com>
2022-04-30 20:19:26 +0200

    add my form to do vue componentes to search-box

--- src/components/search-box.vue
+++ src/components/search-box.vue
@@ -1,80 +1,39 @@
-<template>
-  <div class="search-box">
-    <input
-      list="recomended-searches"
-      class="search-box__input"
-      type="search"
-      v-model="searchByUserStr"
-      :placeholder="placeholderStr"
-      @input="onQueryChange()"
-    />
-    <datalist id="recomended-searches">
-      <option value=""></option>
-      <option
-        v-for="artist in searchSuggestionList"
-        :key="artist"
-        :value="artist"
-      >
-        {{ artist }}
-      </option>
-    </datalist>
-  </div>
-</template>
+<template src="./search-box.html"></template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import store from "@/store";
 
 export default defineComponent({
   name: "search-box",
-  data() {
-    return {
-      searchByUserStr: "",
-      placeholderStr: "Artist, songs or podcast",
-      searchSuggestionList: store.state.searchSuggestionList,
-    };
-  },
-  methods: {
-    onQueryChange: function () {
-      if (this.searchByUserStr != "") {
-        store.dispatch("search", this.searchByUserStr);
+  props: {},
+  setup(props, { emit }) {
+    const searchByUserRefStr = ref("");
+    const placeholderRefStr = ref("Artist, songs or podcast");
+    const searchSuggestionRefList = ref(store.state.searchSuggestionList);
+
+    const onQueryChange = () => {
+      if (searchByUserRefStr.value != "") {
+        store.dispatch("search", searchByUserRefStr);
       } else {
-        this.cleaningSearch();
+        cleaningSearch();
       }
-    },
-    cleaningSearch: function () {
+    };
+    const cleaningSearch = () => {
       store.commit("setArtists", {});
       store.commit("setAlbums", {});
       store.commit("setTracks", {});
-    },
+    };
+
+    return {
+      searchByUserRefStr,
+      placeholderRefStr,
+      searchSuggestionRefList,
+
+      onQueryChange,
+    };
   },
 });
 </script>
 
-<style lang="scss" scoped>
-.search-box {
-  width: 300px;
-  margin: auto;
-}
-
-.search-box__input {
-  width: 100%;
-  padding: 2%;
-  border-radius: 15px;
-  padding-left: 25px;
-  background: url("https://static.thenounproject.com/png/101791-200.png")
-    no-repeat scroll left center;
-  background-size: 20px;
-  background-color: white;
-}
-
-input[type="search"]::-webkit-search-cancel-button {
-  -webkit-appearance: none;
-  height: 16px;
-  width: 16px;
-  background: url(https://pro.fontawesome.com/releases/v5.10.0/svgs/solid/times-circle.svg)
-    no-repeat 50% 50%;
-  background-size: contain;
-  opacity: 1;
-}
-</style>
+<style lang="scss" src="./search-box.scss"></style>
